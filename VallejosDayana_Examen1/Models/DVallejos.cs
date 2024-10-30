445acb9892c2e8bc3ec745b8de9e2708baff2748
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2024-10-30 09:02:21 -0500

    commit2

--- VallejosDayana_Examen1/Models/DVallejos.cs
+++ VallejosDayana_Examen1/Models/DVallejos.cs
@@ -1,9 +1,13 @@
-﻿namespace VallejosDayana_Examen1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VallejosDayana_Examen1.Models
 {
     public class DVallejos
     {
+        [Required]
         public int Edad { get; set; }
 
+        [Key]
         public string Nombre { get; set; }
         public double Estatura { get; set; }
         public bool EsSoltero { get; set; }
